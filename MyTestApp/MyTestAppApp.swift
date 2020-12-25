40ade13c6b7d4891d8e796fcc51231e6002ea883
abraham <qianyonglian@outlook.com>
2020-12-25 15:49:44 +0800

    Initial Commit

---
+++ MyTestApp/MyTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTestAppApp.swift
+//  MyTestApp
+//
+//  Created by msdn uwp on 2020/12/25.
+//
+
+import SwiftUI
+
+@main
+struct MyTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
